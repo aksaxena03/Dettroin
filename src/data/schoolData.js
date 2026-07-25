ab2a6df95a55417868f21e2867f969d17cc53006
aksaxena03 <aksaxena0333@gmail.com>
2026-07-25 23:13:11 +0530

    initial

--- src/data/schoolData.js
+++
@@ -1,6 +0,0 @@
-export const QUICK_STATS = [
-  { label: 'Students Enrolled', value: '1,500+' },
-  { label: 'Qualified Faculty', value: '120+' },
-  { label: 'Campus Area', value: '15 Acres' },
-  { label: 'Student-Teacher Ratio', value: '12:1' },
-];
